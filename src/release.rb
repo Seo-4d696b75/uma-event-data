c5f9ba153196fdd4f8475bba3230e02078225325
k_senda <k_senda@yumemi.co.jp>
2022-09-12 22:36:29 +0900

    [update] サポ＆育成キャラid確認テスト

--- src/release.rb
+++ src/release.rb
@@ -1,5 +1,6 @@
 require 'json'
 require 'date'
+require 'set'
 
 def read_json(path)
   str = ''
@@ -25,33 +26,74 @@ event = read_json('src/event.json').map do |e|
   }
 end
 
+def assert(condition, message='')
+  return if condition
+
+  puts message
+  exit(1)
+end
+
+old_owner = read_json('data.json')['owner']
+ids = Set.new
+max_id = (old_owner['chara'] + old_owner['support']).map do |e|
+  assert !ids.member?(e['id']), "id duplicated #{JSON.dump(e)}"
+  ids.add(e['id'])
+  e['id']
+end.max
+
 owner = read_json('src/icon.json')
-idx = 0
 owner['chara'].map! do |c|
+  name = c['n']
   i = c['i']
   list = [i]
   second = "#{File.basename(i, '.png')}_e.png"
   list << second if File.exist?("icon/#{second}")
 
+  idx = old_owner['chara'].index { |e| e['name'] == name }
+  id = max_id + 1
+  if idx
+    id = old_owner['chara'].delete_at(idx)['id']
+  else
+    puts "new chara detected #{JSON.dump(c)}"
+    max_id += 1
+  end
+
   {
-    'id' => (idx += 1),
-    'name' => c['n'],
+    'id' => id,
+    'name' => name,
     'icon' => list
   }
 end
+assert old_owner['chara'].length.zero?, "chara not found #{JSON.dump(old_owner['chara'])}"
+
 owner['support'].map! do |s|
+  name = s['n']
+  type = s['l'][0..1]
+  rearity = s['l'].match(/S{0,2}R/)[0]
+  idx = old_owner['support'].index do |e|
+    e['name'] == name && e['type'] == type && e['rearity'] == rearity
+  end
+  id = max_id + 1
+  if idx
+    id = old_owner['support'].delete_at(idx)['id']
+  else
+    puts "new support detected #{JSON.dump(s)}"
+    max_id += 1
+  end
+
   {
-    'id' => (idx += 1),
-    'name' => s['n'],
+    'id' => id,
+    'name' => name,
     'icon' => s['i'],
-    'type' => s['l'][0..1],
-    'rearity' => s['l'].match(/S{0,2}R/)[0]
+    'type' => type,
+    'rearity' => rearity
   }
 end
+assert old_owner['support'].length.zero?, "support not found #{JSON.dump(old_owner['support'])}"
 
 data = {
   'event' => event,
-  'owner' => owner,
+  'owner' => owner
 }
 
 File.open('data.json', 'w') do |file|
