088b29aa51e5505e9008779acb1b70e452ffe723
k_senda <k_senda@yumemi.co.jp>
2022-09-12 21:20:04 +0900

    [move] src/*rb

---
+++ src/release.rb
@@ -0,0 +1,68 @@
+require 'json'
+require 'date'
+
+def read_json(path)
+  str = ''
+  File.open(path, 'r:utf-8') do |f|
+    f.each_line do |line|
+      str << line
+    end
+  end
+  JSON.parse(str)
+end
+
+event = read_json('src/event.json').map do |e|
+  {
+    'title' => e['e'],
+    'title_kana' => e['k'],
+    'owner' => e['n'],
+    'choices' => e['choices'].map do |c|
+      {
+        'name' => c['n'],
+        'message' => c['t']
+      }
+    end
+  }
+end
+
+owner = read_json('src/icon.json')
+owner['support'].map! do |s|
+  {
+    'name' => s['n'],
+    'type' => s['l'],
+    'icon' => s['i']
+  }
+end
+owner['chara'].map! do |c|
+  i = c['i']
+  list = [i]
+  second = "#{File.basename(i, '.png')}_e.png"
+  list << second if File.exist?("icon/#{second}")
+
+  {
+    'name' => c['n'],
+    'icon' => list
+  }
+end
+
+data = {
+  'event' => event,
+  'owner' => owner,
+}
+
+File.open('data.json', 'w') do |file|
+  file.write(JSON.pretty_generate(data))
+end
+
+size = File.size('data.json')
+d = Date.today
+v = d.year * 10_000 + d.month * 100 + d.day
+
+info = {
+  'version' => v,
+  'size' => size
+}
+
+File.open('info.json', 'w') do |file|
+  file.write(JSON.pretty_generate(info))
+end
---
+++ src/test.rb
@@ -0,0 +1,60 @@
+require 'minitest/autorun'
+require 'json'
+
+def read_json(path)
+  str = ''
+  File.open(path, 'r:utf-8') do |f|
+    f.each_line do |line|
+      str << line
+    end
+  end
+  JSON.parse(str)
+end
+
+## データのテスト
+class FormatTest < Minitest::Test
+  def test_event
+    events = read_json('src/event.json')
+    events.each_with_index do |e, i|
+      title = e['e']
+      owner = e['n']
+      kana = e['k']
+      choices = e['choices']
+      assert title.is_a?(String), "invalid title string at index #{i} in event.json"
+      assert owner.is_a?(String), "invalid owner string at index #{i} in event.json"
+      assert kana.is_a?(String), "invalid title kana string at index #{i} in event.json"
+      assert choices.is_a?(Array), "invalid choices array at index #{i} in event.json"
+      choices.each_with_index do |c, j|
+        name = c['n']
+        mes = c['t']
+        assert name.is_a?(String), "invalid name string at index #{j} in event at index #{i} in event.json"
+        assert mes.is_a?(String), "invalid mes string at index #{j} in event at index #{i} in event.json"
+      end
+    end
+  end
+
+  def test_icon
+    data = read_json('src/icon.json')
+    chara = data['chara']
+    support = data['support']
+    assert chara.is_a?(Array), 'invalid "chara" array  in icon.json'
+    assert support.is_a?(Array), 'invalid "support" array in icon.json'
+    support.each_with_index do |s, i|
+      name = s['n']
+      type = s['l']
+      icon = s['i']
+      assert name.is_a?(String), "invalid name string at index #{i} of support in icon.json"
+      assert type.is_a?(String), "invalid type string at index #{i} of support in icon.json"
+      assert icon.is_a?(String), "invalid icon string at index #{i} of support in icon.json"
+      assert File.exist?("icon/#{icon}"), "icon image not found: #{icon} at index #{i} of support in icon.json"
+    end
+
+    chara.each_with_index do |c, i|
+      name = c['n']
+      icon = c['i']
+      assert name.is_a?(String), "invalid name string at index #{i} of chara in icon.json"
+      assert icon.is_a?(String), "invalid icon string at index #{i} of chara in icon.json"
+      assert File.exist?("icon/#{icon}"), "icon image not found: #{icon} at index #{i} of chara in icon.json"
+    end
+  end
+end
